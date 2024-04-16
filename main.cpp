448ffd6add68a4fc6802e64b7f91fb92c03ca672
snonow <arnowilhelm3@icloud.com>
2024-04-16 13:25:08 +0200

    Mise à jour et debut de la question 1

--- main.cpp
+++ main.cpp
@@ -3,30 +3,47 @@
 #include <sstream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 // Structure pour représenter un exemple avec ses attributs et sa classe
-struct Exemple {
+struct S_app {
     std::vector<std::string> attributs;
-    std::string classe;
+    std::vector<std::vector<std::string> > valeurs_possibles;
+    std::vector<std::vector<std::string> > data;
 };
 
-// Lecture des données d'apprentissage depuis un fichier CSV
-std::vector<Exemple> lire_donnees_apprentissage(const std::string& nom_fichier) {
+S_app lire_donnees_apprentissage(const std::string& nom_fichier) {
     std::ifstream fichier(nom_fichier);
-    std::vector<Exemple> exemples;
+    S_app donnees_d_apprentissage;
     std::string ligne;
+
+    // Lecture de la première ligne et récupération des attributs
+    std::getline(fichier, ligne);
+    std::istringstream iss(ligne);
+    std::string attribut;
+    while (std::getline(iss, attribut, ',')) {
+        donnees_d_apprentissage.attributs.push_back(attribut);
+        donnees_d_apprentissage.valeurs_possibles.push_back(std::vector<std::string>());
+    }
+
+    // Lecture de tous les cas
     while (std::getline(fichier, ligne)) {
         std::istringstream iss(ligne);
-        Exemple exemple;
-        std::string attribut;
-        while (std::getline(iss, attribut, ',')) {
-            exemple.attributs.push_back(attribut);
+        std::vector<std::string> vec;
+        std::string valeur;
+        int count = 0;
+        while (std::getline(iss, valeur, ',')) {
+            vec.push_back(valeur);
+            // On ajoute la valeur au vecteur correspondant à l'attribut s'il 
+            // n'en fait pas déjà partie
+            if (std::find(donnees_d_apprentissage.valeurs_possibles[count].begin(), donnees_d_apprentissage.valeurs_possibles[count].end(), valeur) == donnees_d_apprentissage.valeurs_possibles[count].end()) {
+                donnees_d_apprentissage.valeurs_possibles[count].push_back(valeur);
+            }
+            count++;
         }
-        exemple.classe = exemple.attributs.back();
-        exemple.attributs.pop_back(); // Retirer la classe des attributs
-        exemples.push_back(exemple);
+        donnees_d_apprentissage.data.push_back(vec);
     }
-    return exemples;
+    return donnees_d_apprentissage;
 }
 
 // Construction de l'arbre ID3
@@ -36,9 +53,11 @@ std::vector<Exemple> lire_donnees_apprentissage(const std::string& nom_fichier) 
 // À implémenter
 
 int main() {
-    std::string nom_fichier_apprentissage = "Sapp.csv";
-    std::vector<Exemple> exemples = lire_donnees_apprentissage(nom_fichier_apprentissage);
-    
+
+    S_app golf = lire_donnees_apprentissage("donnees/golf.csv");
+
+
+    cout << golf;
     // Construction de l'arbre ID3
     // À faire
 
@@ -47,3 +66,5 @@ int main() {
     
     return 0;
 }
+
+
